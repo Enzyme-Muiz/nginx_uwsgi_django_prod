901051e7435d8d9586cd3e27051cfda5fd3d991e
Muizdeen Raji <muizdeenraji@gmail.com>
2022-04-06 14:23:13 +0100

    secret_key_comment removed

--- django_nginx_docker/django_docker_nginx/django_docker_nginx/settings/base.py
+++ django_nginx_docker/django_docker_nginx/django_docker_nginx/settings/base.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
-#SECRET_KEY = 'django-insecure-%0))f1x%4e*0)jdkx-e1hvh&qy^u_%*&87cy!kb&a+)e&3jw%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
